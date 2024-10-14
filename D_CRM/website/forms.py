0f98d280f033c7b3f30dc5c3bc52649e603718bd
islam <islamamr2003@gmail.com>
2024-10-14 20:38:38 +0300

    Final Commit(For now(

--- D_CRM/website/forms.py
+++ D_CRM/website/forms.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label= "", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
-    first_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(label="", max_lenght=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
 
     class Meta:
         model = User
@@ -32,3 +33,15 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].label = ''
         self.fields[
             'password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}), label="")
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}), label="")
+    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}), label="")
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), label="")
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}), label="")
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}), label="")
+
+    class Meta:
+        model = Record
+        exclude = ('user',)
--- D_CRM/website/urls.py
+++ D_CRM/website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>/', views.customer_record, name='customer-record'),
+    path('record/<int:pk>/delete/', views.delete_record, name='delete-record'),
+    path('add_record/', views.add_record, name='add-record'),
+    path('record/<int:pk>/update/', views.update_record, name='update-record'),
 ]
